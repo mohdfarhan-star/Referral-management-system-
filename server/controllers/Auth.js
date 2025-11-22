0d5d7f22d20bf9095ca118a6f3da1b293847062d
Mohd Konain <79141770+mohdfarhan-star@users.noreply.github.com>
2025-11-22 16:40:43 +0530

    Add secure and sameSite options to cookie

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -112,6 +112,8 @@ exports.login = async (req, res) => {
         const options = {
           expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
           httpOnly: true,
+          sameSite:"none",
+          secure:true,
         }
         res.cookie("token", token, options).status(200).json({
           success: true,
